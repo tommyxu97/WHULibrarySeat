67c0b58e7096554dc47ac5931ba315532f3c3a59
TommyXu97 <377935382@qq.com>
2019-02-05 21:06:54 +0800

    完成登录界面的UI

--- app/src/main/java/com/xht97/whulibraryseat/ui/activity/LoginActivity.java
+++ app/src/main/java/com/xht97/whulibraryseat/ui/activity/LoginActivity.java
@@ -6,17 +6,15 @@ import android.os.Bundle;
 import com.xht97.whulibraryseat.R;
 import com.xht97.whulibraryseat.base.BaseActivity;
 import com.xht97.whulibraryseat.base.BasePresenter;
+import com.xht97.whulibraryseat.contract.LoginContract;
+import com.xht97.whulibraryseat.presenter.LoginPresenter;
 
 
-public class LoginActivity extends BaseActivity {
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+public class LoginActivity extends BaseActivity implements LoginContract.ILoginView{
 
     @Override
     protected void initView() {
+        // 初始化所有界面上的控件
         setContentView(R.layout.activity_login);
 
 
@@ -24,11 +22,41 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-
+        // 初始化数据
     }
 
     @Override
     protected BasePresenter createPresenter() {
+        return new LoginPresenter();
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void showError() {
+
+    }
+
+    @Override
+    public void showEmptyView() {
+
+    }
+
+    @Override
+    public String getUserId() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
         return null;
     }
 }
